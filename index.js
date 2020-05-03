efa554e7e9f36d1c621056b08d27a0b02787c7db
José Lastra <lastrafda@gmail.com>
2020-05-03 14:00:12 -0400

    feat: remove a person endpoint

--- index.js
+++ index.js
@@ -33,8 +33,24 @@ app.get('/api/persons', (req, res) => {
 	res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const person = persons.find(p => p.id === id)
+	if (person) {
+		res.json(person)
+	} else {
+		res.status(404).end()
+	}
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	persons = persons.filter(p => p.id !== id)
+
+	res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
-	
 })
