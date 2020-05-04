b3269e856eadde03604d41324af81802cd92c0a3
José Lastra <lastrafda@gmail.com>
2020-05-04 14:20:44 -0400

    uibuild

---
+++ build/precache-manifest.8ec7460ae003456e9278ba87c1d22aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8356393d838b5f8618adff2b3afcb7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8297e3223da8a1293813",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fb97e6c62ef0f7ab85a1",
+    "url": "/static/js/2.ce8ccfef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce8ccfef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8297e3223da8a1293813",
+    "url": "/static/js/main.2f2587d4.chunk.js"
+  },
+  {
+    "revision": "bccee6d650832870a352",
+    "url": "/static/js/runtime-main.5401d62a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', { stream: accessLogStream }))
 
